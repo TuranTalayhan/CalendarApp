87af766912c6ff4c0cff3df79f61897654360409
TuranTalayhan <tura0015@gmail.com>
2025-03-04 09:15:08 +0100

    style: add background color to TemperatureView

--- CalendarApp/Views/TemperatureView.swift
+++ CalendarApp/Views/TemperatureView.swift
@@ -11,26 +11,34 @@ struct TemperatureView: View {
     let temperature: Double
 
     var body: some View {
-        if temperature <= 0 {
-            Text("❄️")
-
-        } else if temperature <= 10 {
-            Text("☁️")
-
-        } else if temperature <= 20 {
-            Text("🌤")
-
-        } else if temperature <= 30 {
-            Text("☀️")
-
-        } else if temperature <= 35 {
-            Text("🔥")
-
-        } else {
-            Text("🏜")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(Color.blue.opacity(0.2))
+            VStack {
+                if temperature <= 0 {
+                    Text("❄️")
+                    
+                } else if temperature <= 10 {
+                    Text("☁️")
+                    
+                } else if temperature <= 20 {
+                    Text("🌤")
+                    
+                } else if temperature <= 30 {
+                    Text("☀️")
+                    
+                } else if temperature <= 35 {
+                    Text("🔥")
+                    
+                } else {
+                    Text("🏜")
+                }
+                
+                Text("\(temperature, specifier: "%.1f") °C")
+            }
+            .padding()
         }
-
-        Text("\(temperature, specifier: "%.1f") °C")
+        .fixedSize()
     }
 }
 
