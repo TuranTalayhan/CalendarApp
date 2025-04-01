bc1cd9b1d09836d2870b54a9775b86d332dc5d26
TuranTalayhan <tura0015@gmail.com>
2025-04-01 14:52:46 +0200

    refactor: create firebaseService variable for readability in CalendarApp

--- CalendarApp/CalendarApp.swift
+++ CalendarApp/CalendarApp.swift
@@ -22,6 +22,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 struct CalendarApp: App {
     @State var isLoggedIn = false
     @State var hasLoaded = false
+    private let firebaseService = FirebaseService.shared
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
@@ -42,14 +43,14 @@ struct CalendarApp: App {
         WindowGroup {
             ContentView(isLoggedIn: $isLoggedIn, hasLoaded: $hasLoaded)
                 .onAppear {
-                    FirebaseService.shared.addAuthStateListener { user in
-                        FirebaseService.shared.currentUser = user
+                    firebaseService.addAuthStateListener { user in
+                        firebaseService.currentUser = user
                         isLoggedIn = user != nil
                         hasLoaded = true
                     }
                 }
                 .onDisappear {
-                    FirebaseService.shared.removeAuthStateListener()
+                    firebaseService.removeAuthStateListener()
                 }
         }
         .modelContainer(sharedModelContainer)
