86a37c291eabb820c17edf4dfd7074aebad15198
TuranTalayhan <tura0015@gmail.com>
2025-04-02 17:39:33 +0200

    feat: allow users to specify a group when creating an event

---
+++ CalendarApp/Views/GroupPicker.swift
@@ -0,0 +1,18 @@
+//
+//  GroupPicker.swift
+//  CalendarApp
+//
+//  Created by Turan Talayhan on 02/04/2025.
+//
+
+import SwiftUI
+
+struct GroupPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupPicker()
+}
